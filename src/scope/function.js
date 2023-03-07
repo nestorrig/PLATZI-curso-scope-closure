c89402f99b9d189be41b59bc0b9bef52b29f1028
nestorrg18 <neriga10@hotmail.com>
2023-03-07 01:05:18 -0600

    Clase 3: Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,11 @@
+function greeting() {
+    let userName = "Nestor";
+    console.log(userName);
+
+    if(userName === "Nestor"){
+        console.log(`Hello ${userName}`);
+    }
+}
+
+greeting();
+console.log(userName);
