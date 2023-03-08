fd6942f48ecd1afa2885337baa45e7bc0158bcb4
nestorrg18 <neriga10@hotmail.com>
2023-03-07 20:40:00 -0600

    Clase 6: Strict Mode

---
+++ src/scope/strict-mode.js
@@ -0,0 +1,28 @@
+/// without strict mode
+pi = 3.1416;
+console.log(pi);
+
+function without() {
+    return pi = 3.1416;
+}
+console.log(without());
+
+/// with strict mode
+"use strict";
+pi = 3.1416;
+console.log(pi); // pi is not defined
+
+function without() {
+    "use strict";
+    return pi = 3.1416;
+}
+console.log(without()); // pi is not defined
+
+// El modo estricto tiene varios cambios en la semántica normal de JavaScript:
+
+// 1. Elimina algunos errores silenciosos de JavaScript cambiándolos para que lancen errores.
+
+// 2. Corrige errores que hacen difícil para los motores de JavaScript realizar optimizaciones:
+    // a veces, el código en modo estricto puede correr más rápido que un código idéntico pero no estricto.
+
+// 3. Prohíbe cierta sintaxis que probablemente sea definida en futuras versiones de ECMAScript.
