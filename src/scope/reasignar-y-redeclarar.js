4c88b23872c7e120e40aec255cabee6e695ffcc3
nestorrg18 <neriga10@hotmail.com>
2023-03-07 20:29:00 -0600

    Clase 5: Reasignación y redeclaración

---
+++ src/scope/reasignar-y-redeclarar.js
@@ -0,0 +1,37 @@
+/// VAR 
+var firstname; // undefined
+firstname = "Juan"
+console.log(firstname);
+//
+var lastname = "Jesus"; // declarar y asignar
+lastname = "Carlos"; // reasignar
+console.log(lastname);
+//
+var secondname = "Pedro"; // declarar y asignar
+var secondname = "Lucas"; // redeclarar y asignar
+console.log(secondname); 
+
+//
+
+// Con var
+var nombre // declaración (undefined) ✅
+nombre = "Andres" // asignación ✅
+nombre = "Valeria" // reasignación ✅
+var nombre = "Oscar" // redeclaración y reasignación ✅
+
+// Con let
+let nombre // declaración (undefined) ✅
+nombre = "Andres" // asignación ✅
+nombre = "Valeria" // reasignación ✅
+let nombre = "Oscar" // SyntaxError: Identifier 'nombre' has already been declared. ❌
+
+// Con const 
+const nombre // SyntaxError: Missing initializer in const declaration. ❌
+const nombre = "Andres" // declaración y asignación ✅
+nombre = "Valeria" // TypeError: Assignment to constant variable. ❌
+const nombre = "Oscar" // SyntaxError: Identifier 'nombre' has already been declared. ❌
+// mutabilidad
+const array = ["manzana", "pera"] // declaración y asignación ✅
+array[0] = "piña" // Cambio de valor en un elemento del array 
+console.log(array) // [ 'piña', 'pera' ]
+const array = [1,2,3] // SyntaxError: Identifier 'array' has already been declared. ❌
